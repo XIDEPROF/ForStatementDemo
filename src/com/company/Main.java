e94621371b215f1bcfa402dcb2b0938a91f2db49
Serge <78102903+MrXides@users.noreply.github.com>
2021-11-01 16:37:26 +0300

    T-01
    ForStatementDemo

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int count;
+	for (count = 1;count < 6;count++){
+	    System.out.println("count = " + count);
+    }
+    }
+}
